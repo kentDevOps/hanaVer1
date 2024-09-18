6d0421b41143a50bd16dde83cff61236cff7ede3
kentDevOps <data.quaza.3@gmail.com>
2024-09-18 17:43:57 +0700

    dang sua common.py

--- common.py
+++ common.py
@@ -22,7 +22,9 @@ def BOMprocess():
     print(df_None_Kd)   
     # Đọc file miễn thuế để lấy ra cột tên nguyên liệu
     df_loc_mienThue = mienThueProcess()
-    #lấy cột mã NPL từ df_kd
+    # Đọc file Đóng thuế và TC để lấy ra cột tên nguyên liệu
+    df_loc_dongThue = mienThueProcess()
+    #lấy cột mã NPL từ df_None_kd
     ma_npl_df_kd = df_None_Kd.iloc[:,1].rename('npl')
     print('df_loc_mienThue:') 
     print(df_loc_mienThue)
@@ -35,6 +37,11 @@ def BOMprocess():
     df_mienThue_tenHang = pd.merge(df_None_Kd,df_loc_mienThue,on='Mã NPL',how='left')
     print('df_mienThue_tenHang:')
     print(df_mienThue_tenHang)
+    #lọc lấy tên hàng theo ma_npl_df_kd từ df_loc_dongThue
+    #lấy cột mã NPL từ df_kd
+    ma_npl_df_kd = df_Kd.iloc[:,1].rename('npl')
+    print('df_loc_mienThue:') 
+    print(df_loc_mienThue)
     '''colNpl =result.iloc[:,1]
     colSoLuong =result.iloc[:,2]
     dfReport = pd.read_excel('temp.xlsx',sheet_name='rp')
@@ -55,3 +62,9 @@ def mienThueProcess():
 def dongThue_Tc_Process():
     file_path_dongThue = getRelativeFile('dongThue','\*dongThue*.xlsx')
     file_path_Tc = getRelativeFile('tc','\*TC*.xlsx')
+    df_dongThue = pd.read_excel(file_path_dongThue[0]).iloc[2:,[3,45]]
+    df_dongThue.columns = ['Mã NPL','tenHang']
+    df_tc = pd.read_excel(file_path_Tc[0]).iloc[:,[8,10]]
+    df_tc.columns = ['Mã NPL','tenHang']
+    df_merged = pd.merge(df_dongThue, df_tc, on='Mã NPL', how='left')
+    return df_merged
